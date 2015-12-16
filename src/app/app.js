257f0accd00e7f5044949f8caf6e3ed716333b05
Vladimir Lugovsky <lugovsky.v@gmail.com>
2015-12-17 00:42:45 +0300

    refactor(app): blurAdmin app completely removed

--- src/app/app.js
+++ src/app/app.js
@@ -1,30 +1,14 @@
 'use strict';
 
-var blurAdminApp = angular.module('BlurAdmin', [
+angular.module('BlurAdmin', [
   'ui.sortable',
-  'ngRoute',
+  'ui.router',
   'ngTouch',
-  'BlurAdmin.theme',
-  'BlurAdmin.dashboard',
-  'BlurAdmin.mail',
-  'BlurAdmin.buttonsPage',
-  'BlurAdmin.gridPage',
-  'BlurAdmin.amChartsPage',
-  'BlurAdmin.chartJsPage',
-  'BlurAdmin.alertsPage',
-  'BlurAdmin.formInputsPage',
-  'BlurAdmin.formLayoutsPage',
-  'BlurAdmin.iconsPage',
-  'BlurAdmin.mapsPage',
-  'BlurAdmin.progressBarsPage',
-  'BlurAdmin.notificationsPage',
-  'BlurAdmin.modalsPage',
-  'BlurAdmin.profilePage',
-  'BlurAdmin.tablesPage',
-  'BlurAdmin.typographyPage',
   'toastr',
   'smart-table',
-  'ui.slimscroll'
-]).config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/dashboard'});
-}]);
+  'ui.slimscroll',
+
+  'BlurAdmin.theme',
+  'BlurAdmin.pages',
+  'BlurAdmin.tplSkin'
+]);
