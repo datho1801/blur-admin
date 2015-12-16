cb1b8b694063cba63b932df62aa4d5c1b5e77b36
alex <demeshko.alexander.v@gmail.com>
2015-12-16 18:23:29 +0300

    chart.js: init

--- src/app/app.js
+++ src/app/app.js
@@ -2,13 +2,29 @@
 
 var blurAdminApp = angular.module('BlurAdmin', [
   'ui.sortable',
-  'ui.router',
+  'ngRoute',
   'ngTouch',
   'BlurAdmin.theme',
-  'BlurAdmin.pages',
+  'BlurAdmin.dashboard',
+  'BlurAdmin.mail',
+  'BlurAdmin.buttonsPage',
+  'BlurAdmin.gridPage',
+  'BlurAdmin.amChartsPage',
+  'BlurAdmin.chartJsPage',
+  'BlurAdmin.alertsPage',
+  'BlurAdmin.formInputsPage',
+  'BlurAdmin.formLayoutsPage',
+  'BlurAdmin.iconsPage',
+  'BlurAdmin.mapsPage',
+  'BlurAdmin.progressBarsPage',
+  'BlurAdmin.notificationsPage',
+  'BlurAdmin.modalsPage',
+  'BlurAdmin.profilePage',
+  'BlurAdmin.tablesPage',
+  'BlurAdmin.typographyPage',
   'toastr',
   'smart-table',
   'ui.slimscroll'
-]).config(['$urlRouterProvider', function ($urlRouterProvider) {
-  $urlRouterProvider.otherwise('/dashboard');
+]).config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/dashboard'});
 }]);
