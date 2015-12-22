bb12fd4455922c0d3df14e269c13350465a67747
alex <demeshko.alexander.v@gmail.com>
2015-12-22 16:11:17 +0300

    sliders page

--- src/app/theme/components/contentTop/contentTop.directive.js
+++ src/app/theme/components/contentTop/contentTop.directive.js
@@ -32,6 +32,7 @@
           '/profile': 'User Profile',
           '/tables': 'Tables',
           '/tree': 'Tree View',
+          '/slider': 'Sliders',
           '/typography': 'Typography',
           '/form-layouts': 'Form Layouts',
           '/form-inputs': 'Form Inputs',
